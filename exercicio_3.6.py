8b63f9e9b91257d063bf892e4a9abc0597d841cd
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2025-03-07 14:10:36 -0300

    Add files via upload

---
+++ exercicio_3.6.py
@@ -0,0 +1,19 @@
+"""
+Programa: parte 3. exercicio 6
+Descrição: imprima na tela todos os n´umeros de 1 a 100.
+Autor: Vinicius Garcia
+Versão: 0.0.1
+Data: 03/03/2025
+"""
+# Alocação de memória
+i=0
+fim=0
+
+# Entrada de dados
+i=1
+fim = 100
+
+# Processamento e saída de dados
+while i <= fim:
+    print(i)
+    i += 1 # significa que i = i + 1
