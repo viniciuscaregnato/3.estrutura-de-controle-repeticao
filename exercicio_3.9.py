3ac5409e292c7ad2294948a7032268698068b1a0
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2025-03-07 23:05:57 -0300

    Add files via upload

---
+++ exercicio_3.9.py
@@ -0,0 +1,20 @@
+"""
+Programa: parte 3. exercicio 9
+Descrição:  leia 6 numeros e imprima o cubo e a raiz c´ubica de cada um deles.
+Autor: Vinicius Garcia
+Versão: 0.0.1
+Data: 03/03/2025
+"""
+# Alocação de memória
+numeros = []
+
+# Entrada de dados
+
+
+# Processamento e saída de dados
+
+
+
+# Saida de dados
+
+
