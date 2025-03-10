b1c411c0be596462426ebd68327082b908b57eda
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2025-03-10 12:43:34 -0300

    Add files via upload

---
+++ exercicio_3.4.py
@@ -0,0 +1,16 @@
+"""
+Programa: parte 3. exercicio 4
+Descrição: imprima na tela todos os n´umeros pares entre 0 e 20
+Autor: Vinicius Garcia
+Versão: 0.0.1
+Data: 03/03/2025
+"""
+
+
+# entrada de dados
+
+
+# armazenamento de dados
+
+
+# processamento e saida de dados
